74de2ecdbad56e44fcdf0006ec490ca604ab13cb
-Jao- <112592702+joaofical@users.noreply.github.com>
2024-10-03 08:39:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,15 +6,36 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [
     {
         enunciado: "Você ja ouviu falar sobre a Inteligência Artificial (IA)?",
-        alternativas:["Sim","Não"]
+        alternativas:[{
+            texto:"Sim",
+            afirmativa:"Afirmativa da alternativa 1"},
+            {
+                texto:"Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
+        ]
     },
     {
         enunciado: "Você percebe a IA na sua vida cotidiana?",
-        alternativas:["Sim","Não"]
+        alternativas:[{
+            texto:"Sim",
+            afirmativa:"Afirmativa da alternativa 1"},
+            {
+                texto:"Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
+        ]
     },
     {
         enunciado: "Você acha que ela faz diferença na sua vida?",
-        alternativas:["Sim","Não"]
+        alternativas:[{
+            texto:"Sim",
+            afirmativa:"Afirmativa da alternativa 1"},
+            {
+                texto:"Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
+        ]
     }
 ]
 
@@ -29,7 +50,13 @@ function mostraPergunta(){
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativa){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",function(){
+            posicao++;
+            mostraPergunta();
+        });
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
+
+mostraPergunta();
